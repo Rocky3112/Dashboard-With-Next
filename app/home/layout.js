b413df8f47a83e0374ba53454350ac3865b29872
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-01-30 01:03:15 +0600

    sidebar completed

---
+++ app/home/layout.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div>
+      hoiseeeeeeeeeeeeeeee
+    </div>
+  )
+}
+
+export default layout
